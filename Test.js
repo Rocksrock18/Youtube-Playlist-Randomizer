8bf9a1648ce630f15598baf4155c18362353a63f
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2020-06-24 19:46:29 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,3 +1,5 @@
+var videos;
+
 function Randomize()
 {
     var playlistID = document.getElementById('pl').value;
@@ -9,11 +11,13 @@ function Randomize()
         crossDomain: true,
         url: link,
         success: function(data){
-            document.write("<iframe width=\"420\" height=\"315\"src=\"https://www.youtube.com/embed/" + data[0] + "?autoplay=1\"allow=\"autoplay\"></iframe>");
+            //document.write("<iframe width=\"420\" height=\"315\"src=\"https://www.youtube.com/embed/" + data[0] + "?autoplay=1\"allow=\"autoplay\"></iframe>");
             for(var i = 0; i < data.length; i++)
             {
                 console.log("Data Recieved: " + data[i]);
             }
+            videos = data;
+            setVideoList(videos);
         },
         error: function (jqXHR, error, errorThrown) {
             alert(jqXHR.responseText
