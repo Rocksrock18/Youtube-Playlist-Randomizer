b505756651be7620df464d749cb69ab56aebafe1
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2020-06-24 18:57:44 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,12 +1,13 @@
 function Randomize()
 {
     var playlistID = document.getElementById('pl').value;
-    console.log(playlistID);
+    var link = 'https://localhost:44321/api/values?playlistID='+playlistID;
+    console.log("Link: " + link);
     $.ajax({
         type: 'GET',
         data: playlistID,
         crossDomain: true,
-        url: 'https://localhost:44321/api/values',
+        url: link,
         success: function(data){
             document.write("<iframe width=\"420\" height=\"315\"src=\"https://www.youtube.com/embed/" + data[0] + "?autoplay=1\"allow=\"autoplay\"></iframe>");
             for(var i = 0; i < data.length; i++)
