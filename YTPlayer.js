4a24c0380d2445c01d952a2e214924187976c7c6
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2020-06-25 16:32:47 -0400

    Update YTPlayer.js

--- YTPlayer.js
+++ YTPlayer.js
@@ -1,4 +1,5 @@
 var videoList;
+var titleList;
 var nextVideo;
 var index;
 var listLength;
@@ -15,6 +16,34 @@ function onYouTubePlayerAPIReady() {
     });
 }
 
+function findIndexByID(vid)
+{
+    console.log(vid);
+    for(var i = 0; i < videoList.length; i++)
+    {
+        var video = videoList[i];
+        if(video === vid)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function skipToVideo(e)
+{
+    var vid = e.target.id;
+    index = findIndexByID(vid);
+    if(index == -1)
+    {
+        alert("Error: Video not found");
+    }
+    else
+    {
+        playNextVideo(vid);
+    }
+}
+
 function playNextVideo(vid)
 {
     player.loadVideoById(vid)
@@ -27,6 +56,35 @@ function setVideoList(vl)
     index = 0;
     nextVideo = videoList[index];
     playNextVideo(nextVideo);
+}
+
+function initializeQueue()
+{
+    var ol = document.getElementById("dynlist");
+    for(var i = 0; i < titleList.length; i++)
+    {
+        var li = document.createElement("li");
+        li.setAttribute('id', videoList[i]);
+        li.addEventListener("click", skipToVideo);
+        li.appendChild(document.createTextNode(titleList[i]));
+        ol.appendChild(li);
+    }
+}
+
+function destroyQueue()
+{
+    var ol = document.getElementById("dynlist");
+    for(var i = 0; i < videoList.length; i++)
+    {
+        var li = document.getElementById(videoList[i]);
+        ol.removeChild(li);
+    }
+}
+
+function setTitleList(tl)
+{
+    titleList = tl;
+    initializeQueue();
 }
 
 // autoplay video
@@ -45,6 +103,7 @@ function onPlayerStateChange(event) {
         }
         else
         {
+            destroyQueue();
             Randomize();
         }
     }
