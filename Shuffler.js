952701051d8984e1ada730ca6883c58a0046c102
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2020-06-26 00:50:08 -0400

    Update Shuffler.js

--- Shuffler.js
+++ Shuffler.js
@@ -27,3 +27,23 @@ function Randomize()
         }
     });
 }
+
+function Reshuffle()
+{
+    destroyQueue();
+    var videos = GetVideos();
+    var titles = GetTitles();
+    var newVideoList = [];
+    var newTitleList = [];
+    var numVideos = videos.length;
+    for(var i = 0; i < numVideos; i++)
+    {
+        var index = Math.floor(Math.random() * videos.length);
+        newVideoList[i] = videos[index];
+        newTitleList[i] = titles[i];
+        videos.splice(index, 1);
+        titles.splice(index, 1);
+    }
+    setVideoList(newVideoList);
+    setTitleList(newTitleList);
+}
