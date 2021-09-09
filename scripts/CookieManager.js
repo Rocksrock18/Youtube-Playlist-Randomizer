bf9bcfc1a9ddd1ca2fd9f471bfbcbaf2ebae7852
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2021-09-09 13:36:23 -0400

    Update CookieManager.js

--- scripts/CookieManager.js
+++ scripts/CookieManager.js
@@ -1,7 +1,23 @@
 import Cookies from '../js.cookie.mjs'
 
-Cookies.set('foo', 'bar')
+function SetCookie(key, value)
+{
+    Cookies.set(key, value);
+}
 
-alert(Cookies.get('foo'))
+function ReadCookie(key)
+{
+    return Cookies.get(key);
+}
 
-console.log(Cookies.get('foo'))
+function ResumePlay()
+{
+    vl = ReadCookie("videos");
+    tl = ReadCookie("titles");
+    current_id = ReadCookie("id");
+    typeof tl !== 'undefined' && setTitleList(tl);
+    typeof vl !== 'undefined' && setVideoList(vl);
+    typeof current_id !== 'undefined' && playNextVideo(current_id);
+}
+
+ResumePlay();
