265350b6e17582389f65aede1be633caba006ea9
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2021-09-09 17:22:56 -0400

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -10,3 +10,24 @@ function onYouTubePlayerAPIReady() {
     window.player = player;
     console.log("test");
 }
+
+// autoplay video
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// when video ends
+function onPlayerStateChange(event) {        
+    if(event.data === 0) {            
+        index++;
+        if(index != listLength)
+        {
+            nextVideo = videoList[index];
+            playNextVideo(nextVideo);
+        }
+        else
+        {
+            Reshuffle();
+        }
+    }
+}
