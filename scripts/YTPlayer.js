881fb7a7ccceea6b3c4e0ae1206d49a2f4f76708
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2021-09-09 17:22:13 -0400

    Update YTPlayer.js

--- scripts/YTPlayer.js
+++ scripts/YTPlayer.js
@@ -134,25 +134,3 @@ export function Example()
 {
     alert("Here's an example url: https://www.youtube.com/playlist?list=PLpJl5XaLHtLX-pDk4kctGxtF4nq6BIyjg\n\nPlaylist ID: PLpJl5XaLHtLX-pDk4kctGxtF4nq6BIyjg");
 }
-
-// autoplay video
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-// when video ends
-function onPlayerStateChange(event) {        
-    if(event.data === 0) {            
-        index++;
-        if(index != listLength)
-        {
-            nextVideo = videoList[index];
-            playNextVideo(nextVideo);
-        }
-        else
-        {
-            Reshuffle();
-        }
-    }
-}
-window.Example = Example;
