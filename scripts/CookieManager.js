8c8386d3f6b358ebd8a9b8023bd96368673f6899
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2021-09-09 19:53:35 -0400

    Update CookieManager.js

--- scripts/CookieManager.js
+++ scripts/CookieManager.js
@@ -3,26 +3,27 @@ import {destroyQueue, setTitleList, setVideoList, playNextVideo} from '../script
 
 export function SetCookie(key, value)
 {
-    Cookies.remove(key);
-    Cookies.set(key, JSON.stringify(value));
+    localStorage.setItem(key, value)
+    //Cookies.set(key, JSON.stringify(value));
 }
 
 export function ReadCookie(key)
 {
-    return Cookies.get(key);
+    return localStorage.getItem(key);
+    //return Cookies.get(key);
 }
 
 export function ResumePlay()
 {
-    Cookies.set("titles", "Hello world!");
+    //Cookies.set("titles", "Hello world!");
     var vl = ReadCookie("videos");
     var tl = ReadCookie("titles");
     var current_id = ReadCookie("id");
     console.log(vl)
     console.log(tl)
     console.log(current_id)
-    typeof tl !== 'undefined' && setTitleList(JSON.parse(tl));
-    typeof vl !== 'undefined' && setVideoList(JSON.parse(vl));
-    typeof current_id !== 'undefined' && playNextVideo(JSON.parse(current_id));
+    typeof tl !== 'undefined' && setTitleList(tl);
+    typeof vl !== 'undefined' && setVideoList(vl);
+    typeof current_id !== 'undefined' && playNextVideo(current_id);
 }
 window.ResumePlay = ResumePlay;
